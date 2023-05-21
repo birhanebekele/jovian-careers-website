ecaf77219fb6ef961b7fa2a3942fa7a7d8746a20
birhanebekele <54853579+birhanebekele@users.noreply.github.com>
2023-05-21 02:31:42 +0000

    Added Json and API

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
 app = Flask(__name__)
+JOBS = [{
+  'id': 1,
+  'title': 'data analist',
+  'location': 'Addis Ababa',
+  'salary': 'br. 120,000'
+}, {
+  'id': 2,
+  'title': 'front end development',
+  'location': 'Hawassa',
+  'salary': 'br. 130,000'
+}, {
+  'id': 3,
+  'title': 'Database designer',
+  'location': 'Addis Ababa',
+  'salary': 'br. 100,000'
+}, {
+  'id': 4,
+  'title': 'Network designner',
+  'location': 'Arbaminch',
+  'salary': 'br. 150,000'
+}]
+
+
 @app.route("/")
 def Hello():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, campanyname='BBK')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug = True)
+  app.run(host='0.0.0.0', debug=True)
